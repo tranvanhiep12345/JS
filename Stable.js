5ec58a0a358d140319cae3e18249fc5ca79afb5a
Hiep <tranvanhiep992001@gmail.com>
2023-03-09 11:45:31 +0700

    first commit

---
+++ Stable.js
@@ -0,0 +1,6 @@
+inputWeight=prompt("Enter the Weight")
+inputHeight=prompt("Enter the Height ")
+let Weight=parseInt(inputWeight)
+let Height=parseInt(inputHeight)
+let Area=Weight*Height
+document.write("The area is: " + Area);
